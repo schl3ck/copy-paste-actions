744b9a2d236a2625d35c8b02cb5edef3415ba6f4
schl3ck <schleck2@gmx.at>
2020-05-11 18:37:20 +0200

    Reuse workers after they finished

--- src/utils/worker.js
+++ src/utils/worker.js
@@ -6,6 +6,17 @@ const WorkerClasses = {
   merger: Merger
 };
 
+const WorkerCache = {
+  analyser: null,
+  merger: null
+};
+const allWorkers = [];
+
+window.clearCache = function() {
+  allWorkers.forEach(w => w.terminate());
+  WorkerCache.analyser = WorkerCache.merger = null;
+};
+
 /**
  * Calls the specified script multiple times
  * @param {"analyser" | "merger"} script The script to call
@@ -19,21 +30,29 @@ export default function(script, dict, update) {
 
   let totalPercentage = 0;
   const worker = new Promise((resolve, reject) => {
-    const w = new (WorkerClasses[script])();
+    let w;
+    if (WorkerCache[script]) {
+      w = WorkerCache[script];
+    } else {
+      const Worker = WorkerClasses[script];
+      w = WorkerCache[script] = Worker();
+      allWorkers.push(w);
+    }
+
     w.addEventListener("message", (event) => {
-      if (event.data && !event.data.finished) {
+      if ((event.data || event.data === 0) && !event.data.finished) {
         totalPercentage = event.data;
         callUpdate();
       } else {
-        w.terminate();
         totalPercentage = 100;
         callUpdate();
         resolve(event.data.data);
       }
     });
     w.addEventListener("error", (error) => {
+      // eslint-disable-next-line no-console
+      console.error("Error in worker:", error);
       reject(error);
-      w.terminate();
     });
     w.postMessage(dict);
   });
