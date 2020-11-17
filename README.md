7abfdbcdcf5fe57bcde7aa7ab4582f3fd17f1c39
schl3ck <schleck2@gmx.at>
2020-11-17 22:46:26 +0100

    Add info about remote console to readme

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ In the issues section all issues regarding the shortcut are handled, not only th
 If you want to help me improve the shortcut, you can help me fix any open issue or translate it into a new language (or fix existing ones) by creating a pull request.
 
 ## Development
-To develop locally you can use any IDE. I personally use [Visual Studio Code](https://code.visualstudio.com/), but any other editor that can handle Unix line feed characters on Windows (instead of the Windows default carrige return-line feed) is also fine.
+To develop locally you can use any Editor/IDE. I personally use [Visual Studio Code](https://code.visualstudio.com/), but any other editor that can handle Unix line feed characters on Windows (instead of the Windows default carrige return-line feed) is also fine.
 
 ### Install all dependencies
 ```
@@ -43,3 +43,13 @@ npm run generateLanguageFile
 ```
 npm run test -- --all
 ```
+
+## Use the remote console to debug
+On macOS you can easily debug a webpage on iOS with Safari on the Mac, but on Windows that is not possible. There are some workarounds like [this one](https://washamdev.com/debug-a-website-in-ios-safari-on-windows/), but they are not guarnteed to work, so I added a simple script that sends all console messages to a local server.
+
+### Setup
+1. Make sure your Mac/PC and iOS device is in the same network.
+2. Open a console and type `npm run remoteConsoleServer`
+3. Create a file `remoteConsoleUrl.local` in the root of this repo with the URL shown after startup of the previous step (no final line feed character!)
+4. Start the webpack-dev-server with `npm run serve`
+5. Use the website on your iOS device and all custom console messages are send to your Mac/PC
--- vue.config.js
+++ vue.config.js
@@ -4,32 +4,37 @@ module.exports = {
   lintOnSave: "default",
   productionSourceMap: false,
   css: {
-    extract: false
+    extract: false,
   },
   devServer: {
     host: "0.0.0.0",
     port: 8080,
     bonjour: true,
-    disableHostCheck: true
+    disableHostCheck: true,
   },
   chainWebpack(config) {
     if (process.env.BUILD_MODE === "targz") {
-      config.entry("app").clear().add("./src/targz-entry.js");
+      config
+        .entry("app")
+        .clear()
+        .add("./src/targz-entry.js");
     }
 
     config.plugins.delete("preload");
     config.plugins.delete("prefetch");
 
-    config.module.rule("text")
+    config.module
+      .rule("text")
       .test(/\.txt$/i)
       .use("raw-loader")
       .loader("raw-loader");
 
-    config.module.rule("worker")
+    config.module
+      .rule("worker")
       .test(/\.worker\.js$/i)
       .use("worker-loader")
       .loader("worker-loader")
-      .tap(options => {
+      .tap((options) => {
         options = options || {};
         options.inline = "no-fallback";
         return options;
@@ -37,41 +42,42 @@ module.exports = {
       .end()
       .use("babel-loader")
       .loader("babel-loader");
-    config.module.rule("js")
-      .exclude.add(/\.worker\.js/);
+    config.module.rule("js").exclude.add(/\.worker\.js/);
 
     if (config.plugins.has("copy")) {
-      config.plugin("copy")
-        .tap(args => {
-          const options = args[0][0] || {};
-          options.ignore = options.ignore || [];
-          options.ignore.push("favicon.png");
-          return args;
-        });
-    }
-
-    config.plugin("html")
-      .tap(args => {
-        const options = args[0] = args[0] || {};
-        options.REMOTE_CONSOLE_SCRIPT = fs
-          .readFileSync("./src/remoteConsole.js", "utf-8")
-          .replace(/\bREMOTE_CONSOLE_URL\b/g, fs.readFileSync("./remoteConsoleUrl.local", "utf-8").trim());
+      config.plugin("copy").tap((args) => {
+        const options = args[0][0] || {};
+        options.ignore = options.ignore || [];
+        options.ignore.push("favicon.png");
         return args;
       });
+    }
+
+    if (fs.existsSync("./remoteConsoleUrl.local")) {
+      config.plugin("html").tap((args) => {
+        const options = (args[0] = args[0] || {});
+        options.REMOTE_CONSOLE_SCRIPT = fs
+          .readFileSync("./src/remoteConsole.js", "utf-8")
+          .replace(
+            /\bREMOTE_CONSOLE_URL\b/g,
+            fs.readFileSync("./remoteConsoleUrl.local", "utf-8").trim(),
+          );
+        return args;
+      });
+    }
     if (process.env.NODE_ENV === "production") {
-      config.plugin("html")
-        .tap(args => {
-          const options = args[0] = args[0] || {};
-          options.inject = false;
-          options.minify = options.minify || {};
-          options.minify.collapseWhitespace = false;
-          options.minify.removeComments = false;
-          if (process.env.BUILD_MODE === "targz") {
-            options.filename = "targz.html";
-          }
-          options.BUILD_MODE = process.env.BUILD_MODE;
-          return args;
-        });
+      config.plugin("html").tap((args) => {
+        const options = (args[0] = args[0] || {});
+        options.inject = false;
+        options.minify = options.minify || {};
+        options.minify.collapseWhitespace = false;
+        options.minify.removeComments = false;
+        if (process.env.BUILD_MODE === "targz") {
+          options.filename = "targz.html";
+        }
+        options.BUILD_MODE = process.env.BUILD_MODE;
+        return args;
+      });
 
       config.resolve.alias
         .set("bootstrap.css$", "bootstrap/dist/css/bootstrap.min.css")
@@ -83,5 +89,5 @@ module.exports = {
         .set("bootstrap.reboot$", "bootstrap/dist/css/bootstrap-reboot.css")
         .set("bootstrap.js$", "bootstrap/dist/js/bootstrap.bundle.js");
     }
-  }
+  },
 };
